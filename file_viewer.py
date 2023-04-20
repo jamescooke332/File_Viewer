fca9fe7c0f92d44c31939dc7f4f14b1c79779e74
jamescooke332 <104587164+jamescooke332@users.noreply.github.com>
2023-04-20 00:22:05 -0400

    Create file_viewer.py

---
+++ file_viewer.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from turtle import left
+import geopandas as gpd
+from tkinter import filedialog
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import folium
+from folium.plugins import MarkerCluster
+
+
+root = Tk()
+blank_space = " "
+root.title(50* blank_space + "Data Viewer")
+root.resizable(width=FALSE, height = FALSE)
+root.geometry("438x5573+460+40")
+
+# create a canvas widget to display the map
+# create a canvas widget to display the map
+map_frame = Frame(root)
+map_frame.pack(side=BOTTOM, fill=BOTH, expand=1)
+
+
+def import_file():
+    #make open file function 
+    file_path = filedialog.askopenfilename()
+    #create geodataframe
+    gdf = gpd.read_file(file_path)
+     # create figure and axes
+    fig, ax = plt.subplots(figsize=(5,5))
+    # plot the geodataframe
+    gdf.plot(ax=ax)
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side="left", fill=BOTH, expand=1)
+
+
+      # create map centered on the first point in the geodataframe
+    map_center = gdf.geometry.centroid.iloc[0].coords[0][::-1]
+    my_map = folium.Map(location=map_center, zoom_start=10)
+
+    
+def unload():
+    del gdf 
+
+
+def export_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".shp")
+    # do something to create the geodataframe to export
+    gdf.to_file(file_path)
+
+button1 = Button(root, text="Import", command=import_file)
+button1.pack()
+
+button2 = Button(root, text="Export", command=export_file)
+button2.pack()
+
+button3 = Button(root, text="Unload", command=unload)
+button3.pack()
+
+root.mainloop()
+#In this example, the import_file() function opens a file dialog box using the filedialog.askopenfilename() function, which allows the user to select a file. The function then reads in the selected file using geopandas and assigns the resulting geodataframe to the variable gdf. The export_file() function does something similar, but instead uses the filedialog.asksaveasfilename() function to allow the user to select a file to save the exported geodataframe to. Finally, both functions are assigned to their respective buttons using the command option.
+
+
+
+
+
+
